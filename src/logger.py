8eed81264169defb7deb4c3efc182e2134801cef
Rory Kane <roarigami@gmail.com>
2023-07-12 15:04:00 -0400

    small update

--- src/logger.py
+++ src/logger.py
@@ -4,13 +4,13 @@ from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(log_path,exist_ok=True)
+os.makedirs(logs_path,exist_ok=True)
 
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=loggin.INFO,
+    level=logging.INFO,
 
 )
