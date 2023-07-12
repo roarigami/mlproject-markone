042ee1aba24bec5b23a991924b19dc5e4ced0111
Rory Kane <roarigami@gmail.com>
2023-07-12 14:13:22 -0400

    logger update

--- src/logger.py
+++ src/logger.py
@@ -12,6 +12,9 @@ logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=loggin.INFO,
-    
+
 
 )
+
+if __name__=="__main__":
+    logging.info("Logging has started")
