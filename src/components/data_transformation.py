62e1c414a5f676449caa23ff138fc4f2ed529c19
Rory Kane <roarigami@gmail.com>
2023-07-17 16:12:41 -0400

    initiate data transforamation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -20,7 +20,7 @@ class DataTransformation:
     def __init__(self):
         self.data_transformation_config=DataTransformationConfig()
 
-    def get_data_transformer_onject(sekf):
+    def get_data_transformer_object(self):
         try:
             numerical_columns = [
                 "rank", 
@@ -51,8 +51,8 @@ class DataTransformation:
                 ]
             )
             
-            logging.info("Standard scaling of numerical columns is now complete")
-            logging.info("Encoding of categorical columns is now complete")
+            logging.info("Standard scaling of numerical columns is now complete.")
+            logging.info("Encoding of categorical columns is now complete.")
 
             preprocessor=ColumnTransformer(
                 [
@@ -65,3 +65,56 @@ class DataTransformation:
 
         except Exception as e:
             raise CustomException(e,sys)
+
+    def initiate_data_transformation(self,train_path,test_path):
+        try:
+            train_df=pd.read_csv(train_path) 
+            test_df=pd.read_csv(test_path)
+
+            logging.info("Reading of test and train data is now completed.")
+            logging.info("Obtaining preprocessor object.")
+
+            preprocessing_obj=self.get_data_transformer_object()
+
+            target_column_name="profti_margin"
+            numerical_columns = [
+                "rank", 
+                "rank_change",
+                "revenue",
+                "profit",
+                "num_employees"
+            ]
+
+            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df=train_df[target_column_name]
+
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name]
+
+            logging.info(
+                f"Applying preprocessing object on training dataframe and testing dataframe."
+            )
+
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_train_df)
+
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)
+            ]
+            test_arr = np.c_[input_feature_test_arr, np.array(input_feature_test_df)]
+
+            logging.info(f"Saved preprocessing object.")
+
+            save_object(
+                file_path=data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+
+            return(
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
+
+        except:
+            pass 
