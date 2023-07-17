ca11d619ef8dfdff7877546037172a3bbbf94f50
Rory Kane <roarigami@gmail.com>
2023-07-17 11:16:21 -0400

    Data ingestion class

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -13,3 +13,33 @@ class DataIngestionConfig:
     test_data_path: str=os.path.join('artifact', "test.csv")
     raw_data_path: str=os.path.join('artifact', "data.csv")
 
+class DataIngestion: 
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion or component")
+        try:
+            df=pd.read_csv('notebook\data\FortuneRev.csv') 
+            logging.info("Read the dataset as dataframe")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info("Train test split intitiated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("Ingestion of the data is complete")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
