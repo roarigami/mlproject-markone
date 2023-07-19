9675aea20d5bb045251c7511a0a9241b564082b7
Rory Kane <roarigami@gmail.com>
2023-07-18 20:47:54 -0400

    model trainer initiator

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -16,3 +16,39 @@ from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging 
+
+from src.utils import save_object 
+
+@dataclass 
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join("artifacts",'model.pkl')
+
+class ModelTrainer: 
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+
+    def initiate_model_trainer(self,train_array,test_array,preprocessor_path):
+        try:
+            logging.info("Split training and test input data.")
+            x_train,y_train,x_test,y_test=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+            models = {
+                "Random Forest": RandoomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostRegressor(),
+                "Linear Regression": LinearRegression(),
+                "k-Neighbors Classifier": KNeighborsRegressor(),
+                "XGBClassifier": XGBRegressor(),
+                "CatBoosting Classifier": CatBoostRegressor(verbose=false),
+                "AdaBoost Classifier": AdaBoostRegressor(),
+            }
+
+            model_report:dict=evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,
+                                            models=models)
+
+        except:
+            pass
