a260a7964781a08312de0cab71127057f31f29ba
Rory Kane <roarigami@gmail.com>
2023-07-18 21:04:13 -0400

    Evaluate model function

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import dill
 
 import numpy as np 
 import pandas as pd 
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -19,3 +20,29 @@ def save_object(file_path,obj):
 
     except Exception as e:
         raise CustomException(e,sys)
+
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        # X_train, X_test, y_train, y_test = train_test_split(
+        #     X, y, test_size=0.2, random_state=42
+        # )
+
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.valus())[i]
+
+            model.fit(X_train, y_train) ##Train model
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report 
+    
+    except Exception as e: 
+        raise CustomException(e, sys)
