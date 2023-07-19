9bb1de66910ccaa77b5f6c3a6b898840c32f2f7d
Rory Kane <roarigami@gmail.com>
2023-07-18 21:25:29 -0400

    Model trainer update

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -17,7 +17,7 @@ from xgboost import XGBRegressor
 from src.exception import CustomException
 from src.logger import logging 
 
-from src.utils import save_object 
+from src.utils import save_object,evaluate_models
 
 @dataclass 
 class ModelTrainerConfig:
@@ -27,10 +27,10 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
 
-    def initiate_model_trainer(self,train_array,test_array,preprocessor_path):
+    def initiate_model_trainer(self,train_array,test_array):
         try:
             logging.info("Split training and test input data.")
-            x_train,y_train,x_test,y_test=(
+            X_train,y_train,X_test,y_test=(
                 train_array[:,:-1],
                 train_array[:,-1],
                 test_array[:,:-1],
@@ -47,8 +47,31 @@ class ModelTrainer:
                 "AdaBoost Classifier": AdaBoostRegressor(),
             }
 
-            model_report:dict=evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,
+            model_report:dict=evaluate_model(X_train=x_train,y_train=y_train,X_test=x_test,y_test=y_test,
                                             models=models)
 
-        except:
-            pass
+            ## to get best model score from dict 
+            best_model_score = max(sorted(model_report.values()))
+
+            ## To get the best model name from dict 
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name]
+
+            if best_model_score<0.6:
+                raise CustomException("No best mode found.")
+            logging.info("Best model has been found in both training and testing datasets")
+            
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted=best_model.predict(X_test)
+
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+
+        except Exception as e:
+            raise CustomException(e,sys)
